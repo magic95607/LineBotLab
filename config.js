32231be5ce86ed2a90b1849f4e13f8679a465788
陳柏彥 <chenbaiyan@chenbaiyande-MacBook-Pro.local>
2018-07-20 16:33:26 +0800

    Deploying to Heroku

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    line: {
+        channelSecret: '4d66dc4f30065f6b7559791187911ed3',
+        accessToken: 'nZcKskKpGfRvZeHD4iVze5BfbiOQSVIGM1OcxlyjAuqFYtZaJaAJRHxWzrl+7UXyA+aTT4Uff72UoLW8SHgJDqlz2RHzUQ428/TAyFJGFtKv7MtPh/e8nQ8KgAyYPOnwsMxNCFzJ5/hlcelJqem/fQdB04t89/1O/w1cDnyilFU='
+    },
+    messenger: {
+        accessToken: '',
+        appSecret: '',
+        verifyToken: '',
+    },
+    telegram: {
+        accessToken: '',
+    },
+    olami: {
+        appKey: '68aa663fb4544a1fa08819a5e048accf',
+        appSectet: '26ecdc36162c40449b339d8d2addfd2e'
+    },
+    kkbox: {
+        id: 'e14dc858bf3b02aa35f585e9f77fd8ea',
+        secret: '8a48e11b8643b308193debd3e3338a90'
+    }
+}
